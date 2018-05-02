08873adf2bddb6caa18fc3f5fe7cbf0fcbeba363
Thomas Flynn <tflynn049@gmail.com>
2018-05-02 10:32:43 -0400

    1st angular project, loosely based on Tour of Heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Cake Donuts';
+  slogan = 'We only serve the best donuts, and they are cake.';
 }
---
+++ src/app/mock-products.ts
@@ -0,0 +1,18 @@
+import { Product } from './Product';
+
+export const PRODUCTS: Product[] = [
+    { id: 1, name: 'Plain',description: 'Plain Cake Donut', quantityOnHand: 86, price: .75, isInStock: true},
+    { id: 2, name: 'Blueberry',description: 'Blueberry Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 3, name: 'Raspberry',description: 'Raspberry Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 4, name: 'Honey Glazed',description: 'Honey Glazed Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 5, name: 'Chocolate',description: 'ChocolateCake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 6, name: 'Creme Sprinkles',description: 'Cream Sprinkles Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 7, name: 'Orange Glazed',description: 'Orange Glazed Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 8, name: 'Banana',description: 'Banana Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 9, name: 'Licorice',description: 'Licorice Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 10, name: 'Gummy Bear',description: 'Gummy Bear Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 11, name: 'Rhubarb Iced',description: 'Rhubarb Iced Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 12, name: 'Chocolate Fudge',description: 'Chocolate Fudge Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 13, name: 'Cherry',description: 'Cherry filled Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+    { id: 14, name: 'Chocolate Cream Iced',description: 'Chocolate Iced with Cream Filling Cake Donut', quantityOnHand: 120, price: .75, isInStock: true},
+ ];
